47688422b47ea5ed2266b874dbf51ec37a58414a
Maciek Golaszewski <maciej.golaszewski@canonical.com>
2024-07-18 08:53:28 +0200

    integration tests KU-1063

--- tests/sanity/test_rock.py
+++ tests/sanity/test_rock.py
@@ -1,16 +1,19 @@
+#
+# Copyright 2024 Canonical, Ltd.
+# See LICENSE file for licensing details
+#
 import os
 import subprocess
 
 
 def test_sanity():
     image_variable = "ROCK_CONTOUR"
-    entrypoint = "/contour"
+    entrypoint = "contour"
     image = os.getenv(image_variable)
     assert image is not None, f"${image_variable} is not set"
 
     docker_run = subprocess.run(
         ["docker", "run", "--rm", "--entrypoint", entrypoint, image, "--help"],
-
         capture_output=True,
         text=True,
     )
