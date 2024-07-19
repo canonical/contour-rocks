5e5cf623c7c451a26f11193cc6658912ee91701e
Berkay Tekin Oz <berkay.tekinoz@canonical.com>
2024-07-19 13:32:28 +0000

    Revert env rock naming

--- tests/sanity/test_rock.py
+++ tests/sanity/test_rock.py
@@ -7,7 +7,7 @@ import subprocess
 
 
 def test_sanity():
-    image_variable = "ROCK_CONTOUR_V1_28_2"
+    image_variable = "ROCK_CONTOUR"
     entrypoint = "contour"
     image = os.getenv(image_variable)
     assert image is not None, f"${image_variable} is not set"
